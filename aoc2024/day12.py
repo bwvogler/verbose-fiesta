c27c2583a6263800208d95f1dd46f2dedc883299
Brian Vogler <bvogler@generatebiomedicines.com>
2024-12-15 22:02:28 -0500

    Add garden region analysis with perimeter and area calculations

--- aoc2024/day12.py
+++ aoc2024/day12.py
@@ -1,2 +1,55 @@
-with open("aoc2024/data/day11.txt", encoding="utf-8") as f:
-    data = f.read().splitlines()
+import numpy as np
+from scipy.ndimage import label
+
+with open("aoc2024/data/day12.txt", encoding="utf-8") as f:
+    data = np.array([list(line.strip()) for line in f.readlines()])
+
+
+def label_contiguous_regions(array, connectivity=1):
+    structure = (
+        np.ones((3, 3), dtype=int)
+        if connectivity == 2
+        else np.array([[0, 1, 0], [1, 1, 1], [0, 1, 0]], dtype=int)
+    )
+    labeled_array, num_features = label(array, structure)
+    return labeled_array, num_features
+
+
+def list_contiguous_regions(array) -> list[np.ndarray]:
+    labeled_array, num_features = label_contiguous_regions(array)
+    regions = [1 * (labeled_array == label) for label in range(1, num_features + 1)]
+    return regions
+
+
+def perimeter_of_contiguous_region(region: np.ndarray) -> int:
+    region = np.pad(region, 1)
+    vertical_edges = np.sum(region[1:, :] != region[:-1, :])
+    horizontal_edges = np.sum(region[:, 1:] != region[:, :-1])
+    return vertical_edges + horizontal_edges
+
+
+gardens = [1 * (data == plot_label) for plot_label in np.unique(data)]
+
+contiguous_regions = [
+    x for y in [list_contiguous_regions(garden) for garden in gardens] for x in y
+]
+
+perimeters = [perimeter_of_contiguous_region(region) for region in contiguous_regions]
+areas = [np.sum(region) for region in contiguous_regions]
+sum(perimeter * area for perimeter, area in zip(perimeters, areas))
+
+# Part 2
+
+
+def sections_of_perimeter(region: np.ndarray) -> int:
+    region = np.pad(region, 1)
+    top_edges = np.diff(1 * (region[1:, :] > region[:-1, :]), axis=1).sum()
+    left_edges = np.diff(region[:, 1:] > region[:, :-1], axis=0).sum()
+
+    bottom_edges = np.diff(1 * (region[1:, :] < region[:-1, :]), axis=1).sum()
+    right_edges = np.diff(region[:, 1:] < region[:, :-1], axis=0).sum()
+    return top_edges + left_edges + bottom_edges + right_edges
+
+
+sections = [sections_of_perimeter(region) for region in contiguous_regions]
+sum(section * area for section, area in zip(sections, areas))
--- aoc2024/day12_.py
+++ aoc2024/day12_.py
@@ -1,51 +1,78 @@
+from __future__ import annotations
+import numpy as np
+
 with open("aoc2024/data/day12.txt", encoding="utf-8") as f:
-  data = np.array([list(line.strip()) for line in f.readlines()])
+    data = np.array([list(line.strip()) for line in f.readlines()])
 
-class GardenRegion():
-  def __init__(self, label: str, locations: set[np.ndarray])
-    self.locations = locations
 
-  def _directions(self) -> list[np.ndarray]:
-    return [
-      np.array(x, y)
-      for x, y in [(1, 0), (0, -1), (-1,0),(0,1)]
-    ]
+class GardenRegion:
+    def __init__(self, label: str, locations: list[np.ndarray]):
+        self.label = label
+        self.locations: set[tuple[int, int]] = set((x, y) for x, y in locations)
 
-  def is_same(self, other_region: GardenRegion) -> bool:
-    if other_region.label != self.label:
-      return False
-    if any(
-      other_region.location == self.location + direction
-      for direction in self._directions
-    )
+    @property
+    def location_array(self) -> np.ndarray:
+        return np.array(list(self.locations))
 
-  def merge(self, regions: GardenRegion | list[GardenRegion]):
-    if isinstance(regions, GardenRegion):
-      regions = [regions]
-    for region in regions:
-      self.locations.add(region.location)
+    @property
+    def _directions(self) -> list[np.ndarray]:
+        return [np.array((x, y)) for x, y in [(1, 0), (0, -1), (-1, 0), (0, 1)]]
 
-  @property
-  def area(self) -> int:
-    return self.locations.shape[0]
+    def can_merge(self, other_region: GardenRegion) -> bool:
+        if other_region.label != self.label:
+            return False
+        return any(
+            tuple(location) in self.adjacent_locations
+            for location in other_region.location_array
+        )
 
-  @property
-  def perimeter(self) -> int:
-    return sum(
-      location + direction not in self.locations
-      for direction in self._directions
-      for location in self.locations
-    )
+    def merge(self, regions: GardenRegion | list[GardenRegion]):
+        """
+        Merge the current region with one or more other regions.
+
+        Args:
+            regions (GardenRegion | list[GardenRegion]): The region(s) to merge with.
+        """
+        if isinstance(regions, GardenRegion):
+            regions = [regions]
+        for reg in regions:
+            self.locations.update(reg.locations)
+
+    @property
+    def adjacent_locations(self) -> set[np.ndarray]:
+        adjacent_locations_array = np.vstack(
+            [self.location_array + direction for direction in self._directions]
+        )
+        return set(map(tuple, adjacent_locations_array))
+
+    @property
+    def area(self) -> int:
+        return len(self.locations)
+
+    @property
+    def perimeter(self) -> int:
+        return sum(
+            adjacent_location not in self.locations
+            for adjacent_location in self.adjacent_locations
+        )
+
 
 garden = [
-  GardenRegion(data[i, j] for i, j in list(np.ndindex(data.shape))
+    GardenRegion(data[i, j], [np.array((i, j))])
+    for i, j in list(np.ndindex(data.shape))
 ]
 
-while any(same_regionsregion.is_same:
-  same_regions = [
-    other_region
-    for other_region in garden
-    if other_region.is_same(region)
-  ]
-  region.merge(same_regions)
+while any(
+    region.can_merge(other_region) for region in garden for other_region in garden
+):
+    for region in garden:
+        regions_can_merge = [
+            other_region for other_region in garden if region.can_merge(other_region)
+        ]
+        if regions_can_merge:
+            region.merge(regions_can_merge)
+            garden = [g for g in garden if g not in regions_can_merge]
+            print(len(garden))
 
+list(region.perimeter * region.area for region in garden)
+garden[0].locations
