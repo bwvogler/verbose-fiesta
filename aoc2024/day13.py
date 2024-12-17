f17484d135eb06d84c1933d093d24f9b06d1b5e4
Brian Vogler <bvogler@generatebiomedicines.com>
2024-12-16 23:58:18 -0500

    Add test data for day 13 and refactor code to process button and prize information using NumPy

--- aoc2024/day13.py
+++ aoc2024/day13.py
@@ -3,16 +3,37 @@ Button B: X+65, Y+17
 Prize: X=18133, Y=4639
 """
 
+import re
 import numpy as np
 
-a = np.array((11, 73))
-b = np.array((65, 17))
-p = np.array((18133, 4639))
-hunderd = np.array([(100)])
-[
-    3 * a_count + b_count
-    # tuple(a_count * a + b_count * b)
-    for a_count in range(np.hstack([(p // b), hunderd]).min())
-    for b_count in range((p // a).min())
-    if tuple(a_count * a + b_count * b) == tuple(p)
+with open("aoc2024/data/day13.txt") as f:
+    blocks = f.read().split("\n\n")
+
+data = [
+    {
+        key: np.array(
+            [int(x) for x in re.match(r"X[\+=](\d+), Y[\+=](\d+)", value).groups()]
+        )
+        for key, value in (line.split(": ") for line in block.splitlines())
+    }
+    for block in blocks
 ]
+
+hunderd = np.array([(100)])
+results = [
+    min(
+        (
+            3 * a_count + b_count
+            # tuple(a_count * a + b_count * b)
+            for a_count in range(
+                np.hstack([(machine["Prize"] // machine["Button A"]), hunderd]).min()
+            )
+            for b_count in range((machine["Prize"] // machine["Button B"]).min())
+            if tuple(a_count * machine["Button A"] + b_count * machine["Button B"])
+            == tuple(machine["Prize"])
+        ),
+        default=None,
+    )
+    for machine in data
+]
+sum(result for result in results if result is not None)
